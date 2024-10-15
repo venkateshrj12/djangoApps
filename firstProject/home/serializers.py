2c3eda0883118b7f2e85842885e4bfa18a7a7404
Your Name <name@email.com>
2024-10-15 13:48:59 +0530

    added a serialzier for color model and included it in person

--- firstProject/home/serializers.py
+++ firstProject/home/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from home.models import Person, Color
 import re
+import pdb
 
 class ColorSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,7 +9,8 @@ class ColorSerializer(serializers.ModelSerializer):
         fields = ['color_name']
 
 class PeopleSerializer(serializers.ModelSerializer):
-    color = ColorSerializer()
+    color = serializers.PrimaryKeyRelatedField(queryset=Color.objects.all(), required= False)
+
     class Meta:
         model = Person
         # fields = ['name', 'age', ]
@@ -16,12 +18,18 @@ class PeopleSerializer(serializers.ModelSerializer):
         fields = '__all__'
         # depth = 1 # to include nested fields
 
+    def to_representation(self, instance):
+        response = super().to_representation(instance)
+        response['color'] = ColorSerializer(instance.color).data if response['color'] else None
+        return response
 
+# validations for serializers
     # def validate(self, data):
     #     if 'age' in data and data['age'] < 18:
     #         raise serializers.ValidationError({'age': 'should be greater than 18'})
 
     #     return data
+    # Add this method to return full color details in the response
 
     def validate_age(self, age):
         if age < 18:

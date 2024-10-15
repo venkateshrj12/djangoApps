c8beb5d94f02b891873cd671f66bca9ab366d023
Your Name <name@email.com>
2024-10-15 10:56:09 +0530

    validations added to serializers

--- firstProject/api/urls.py
+++ firstProject/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from home.views import courses, people, person
 
-urlpatterns = {
+urlpatterns = [
     path('courses/', courses),
     path('person/<int:id>', person),
     path('people/', people),
-}
+]
--- firstProject/home/serializers.py
+++ firstProject/home/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from home.models import Person
+import re
 
 class PeopleSerializer(serializers.ModelSerializer):
     
@@ -8,3 +9,23 @@ class PeopleSerializer(serializers.ModelSerializer):
         # fields = ['name', 'age', ]
         # exclude = ['name', 'age',]
         fields = '__all__'
+
+    # def validate(self, data):
+    #     if 'age' in data and data['age'] < 18:
+    #         raise serializers.ValidationError({'age': 'should be greater than 18'})
+
+    #     return data
+
+    def validate_age(self, age):
+        if age < 18:
+            raise serializers.ValidationError( 'Age should be greater than 18' )
+
+        return age
+
+    def validate_name(self, name):
+        if len(name) < 3:
+            raise serializers.ValidationError('Name should be greater than 3 characters')
+        if not re.match("^[a-zA-Z0-9 ]*$", name):
+            raise serializers.ValidationError('Name should not contain special characters')
+        
+        return name

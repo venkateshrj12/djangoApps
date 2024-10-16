d11c38c6c79a733e5d4206e3bc7f0420ba4445e6
Your Name <name@email.com>
2024-10-16 22:30:58 +0530

    udpate installed apps with correct syntax

--- social_media/social_media/settings.py
+++ social_media/social_media/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'djangorestframework',
-    'djangorestframework-simplejwt',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'corsheaders',
 ]
 
 SIMPLE_JWT = {
